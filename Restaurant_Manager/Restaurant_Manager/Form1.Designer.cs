df0e8bff10ce72f40e677981b818a1310e16c01a
ndkhang <nguyenduykhang410@gmail.com>
2023-03-12 10:55:41 +0700

    Update login

--- Restaurant_Manager/Restaurant_Manager/Form1.Designer.cs
+++ Restaurant_Manager/Restaurant_Manager/Form1.Designer.cs
@@ -28,32 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUname = new System.Windows.Forms.TextBox();
+            this.txtPasswd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // txtUname
             // 
-            this.button1.Location = new System.Drawing.Point(362, 159);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtUname.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUname.Location = new System.Drawing.Point(171, 89);
+            this.txtUname.Name = "txtUname";
+            this.txtUname.Size = new System.Drawing.Size(240, 23);
+            this.txtUname.TabIndex = 1;
+            // 
+            // txtPasswd
+            // 
+            this.txtPasswd.Location = new System.Drawing.Point(171, 133);
+            this.txtPasswd.Name = "txtPasswd";
+            this.txtPasswd.Size = new System.Drawing.Size(240, 23);
+            this.txtPasswd.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(118, 182);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 5;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(228, 182);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(75, 23);
+            this.btClear.TabIndex = 6;
+            this.btClear.Text = "Clear";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(336, 182);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(75, 23);
+            this.btExit.TabIndex = 7;
+            this.btExit.Text = "Exit";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(191, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(509, 227);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPasswd);
+            this.Controls.Add(this.txtUname);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Button button1;
+        private TextBox txtUname;
+        private TextBox txtPasswd;
+        private Label label2;
+        private Label label3;
+        private Button btLogin;
+        private Button btClear;
+        private Button btExit;
+        private PictureBox pictureBox1;
     }
 }
--- Restaurant_Manager/Restaurant_Manager/Form1.cs
+++ Restaurant_Manager/Restaurant_Manager/Form1.cs
@@ -6,5 +6,27 @@ namespace Restaurant_Manager
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            txtUname.Clear();
+            txtPasswd.Clear();
+            txtUname.Focus();
+        }
+
+        private void txtUname_GotFocus(object sender, EventArgs e)
+        {
+            txtUname.BorderStyle = BorderStyle.FixedSingle;
+        }
     }
 }
